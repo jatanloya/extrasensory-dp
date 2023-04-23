b96502e2f60f5ad78356c5038d6308401512facd
Aadyaa Maddi <aadyaa.maddi@gmail.com>
2023-04-23 12:11:24 -0400

    Add plot generation script

---
+++ repro_visual.py
@@ -0,0 +1,76 @@
+import numpy as np
+import matplotlib.pyplot as plt 
+import seaborn as sns
+
+
+def generate_mia_roc_plot(
+    exp_data_dir, non_private_exp_id, 
+    private_exp_ids, legend_labels, 
+    filename
+):
+    print(filename)
+    fig, ax = plt.subplots()
+
+    for i, label in enumerate(legend_labels):
+        if label == "Non-private":
+            exp_id = non_private_exp_id
+            mia_roc_data_filepath = f"{exp_data_dir}/plots_data/{exp_id}_non_private_mia_roc_data.npz"
+        else:
+            exp_id = private_exp_ids[i - 1]
+            mia_roc_data_filepath = f"{exp_data_dir}/plots_data/{exp_id}_private_mia_roc_data.npz"
+
+        print(mia_roc_data_filepath)
+
+        data = np.load(mia_roc_data_filepath)
+        fpr = data["fpr"]
+        tpr = data["tpr"]
+        ax.plot(fpr, tpr, label=label)
+
+    ax.set_title(f"Performance of Membership Inference Attack")
+    ax.set_xlabel("False Positive Rate")
+    ax.set_ylabel("True Positive Rate")
+
+    ax.legend()
+    plt.savefig(filename, dpi=500)
+
+
+if __name__ == "__main__":
+    exp_data_dir = "exp_data"
+    legend_labels = [
+        "Non-private", 
+        # "Private, eps = 1.0", 
+        # "Private, eps = 0.5", 
+        "Private, eps = 0.125"
+    ]
+
+    non_private_simple_nn_exp_id = "simple_nn_epochs5_eps0_125_delta1e_5_clipnorm1.0"
+    private_simple_nn_exp_ids = [
+        # "simple_nn_epochs5_eps1_0_delta1e_5_clipnorm1.0",
+        # "simple_nn_epochs5_eps0_5_delta1e_5_clipnorm1.0",
+        "simple_nn_epochs5_eps0_125_delta1e_5_clipnorm1.0",
+    ]
+    simple_nn_filename = f"{exp_data_dir}/plots_data/all_simple_nn_mia"
+
+    generate_mia_roc_plot(
+        exp_data_dir=exp_data_dir, 
+        non_private_exp_id=non_private_simple_nn_exp_id,
+        private_exp_ids=private_simple_nn_exp_ids,
+        legend_labels=legend_labels,
+        filename=simple_nn_filename
+    )
+
+    non_private_cnn_exp_id = "cnn_epochs10_eps0_125_delta1e_5_clipnorm1.0"
+    private_cnn_exp_ids = [
+            # "cnn_epochs10_eps1_0_delta1e_5_clipnorm1.0",
+            # "cnn_epochs10_eps0_5_delta1e_5_clipnorm1.0",
+            "cnn_epochs10_eps0_125_delta1e_5_clipnorm1.0",
+            ]
+    cnn_filename = f"{exp_data_dir}/plots_data/all_cnn_mia"
+
+    generate_mia_roc_plot(
+        exp_data_dir=exp_data_dir, 
+        non_private_exp_id=non_private_cnn_exp_id,
+        private_exp_ids=private_cnn_exp_ids,
+        legend_labels=legend_labels,
+        filename=cnn_filename
+    )
